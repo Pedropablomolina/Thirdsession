2142cfc86feb43266ed2a5e289a0843be323b63b
pedropablomolina <ppmolinaarol@gmail.com>
2023-01-26 11:03:49 +0100

    Session 3

---
+++ print_example.py
@@ -0,0 +1,7 @@
+print("This is a text","another text",end="|I want this at the end|\n")
+print("This is a another")
+print("This",2+2,"can","be","written","like","this")
+#
+print("This",2+2,"can","be","written","like","this",sep="¡RACKS!")
+
+#sep is for spaces
